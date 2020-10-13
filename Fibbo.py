fc153b475d85ca52630828668b7b47da346000ad
rishabmattoo <72668951+rishabmattoo@users.noreply.github.com>
2020-10-13 17:52:24 +0530

    Add files via upload

---
+++ Fibbo.py
@@ -0,0 +1,12 @@
+#fibonacci series
+def fib(n):
+    a = 0
+    b = 1
+    print(a)
+    print(b)
+    for i in range(2,n):
+        c = a + b
+        a = b
+        b = c
+        print(c)
+fib(15) 
